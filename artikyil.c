05c01dcfe31f9fe0185c6d4bd50cf436c371b8ed
Saxquatchx <80763619+Saxquatchx@users.noreply.github.com>
2024-10-14 16:30:37 +0300

    Update artikyil.c

--- artikyil.c
+++ artikyil.c
@@ -2,6 +2,20 @@
 
 int main(){
 
+    // 2000-2024 arasını kontrol etme
+
+    for (int i = 2000; i <= 2024; i++)
+    {
+        if (i%4 == 0)
+        {
+            printf("%d bir artik yildir.\n",i);
+        } else {
+            printf("%d bir artik yil degildir.\n", i);
+        }
+    }
+    
+    // Bir yılı veri olarak alıp kontrol etme
+
     int yil;
 
     printf("Yil girin: ");
@@ -13,5 +27,4 @@ int main(){
     } else {
         printf("Artik yil degil");
     }
-    
 }
